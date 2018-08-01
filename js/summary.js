7f9eed08e16b3c45f4f0dbc66bc104585d8f7997
Ann Zhou <ann.zhou@wustl.edu>
2018-08-01 16:24:43 -0500

    issue #15: styled elaboration and hero

--- js/summary.js
+++ js/summary.js
@@ -4,32 +4,32 @@ var sum = Vue.component('sum',{
   template:`
     <div class="hero-italic">
       <div class="row">
-        <div class="text-center">Total Number of Responses: <% total %></div>
+        <div class="text-center">Total Number of Responses: <strong><% total %></strong></div>
         <div class="col-lg-6 col-md-6 col-sm-6 text-center">
-          <lable>Dominant Professional Values</label></br>
-          <p><% pv["0"]["name"] %> <% pv["0"]["percent"] %>%<p>
-          <p><% pv["1"]["name"] %> <% pv["1"]["percent"] %>%<p>
-          <p><% pv["2"]["name"] %> <% pv["2"]["percent"] %>%<p>
+          <label class="pos-label">Dominant Professional Values</label></br>
+          <p><% pv["0"]["name"] %> <strong><% pv["0"]["percent"] %>%</strong><p>
+          <p><% pv["1"]["name"] %> <strong><% pv["1"]["percent"] %>%</strong><p>
+          <p><% pv["2"]["name"] %> <strong><% pv["2"]["percent"] %>%</strong><p>
         </div>
         <div class="col-lg-6 col-md-6 col-sm-6 text-center">
-          <lable>Least Dominant Professional Values</label></br>
-          <p><% pv["8"]["name"] %> <% pv["8"]["percent"] %>%<p>
-          <p><% pv["7"]["name"] %> <% pv["7"]["percent"] %>%<p>
-          <p><% pv["6"]["name"] %> <% pv["6"]["percent"] %>%<p>
+          <label class="neg-label">Least Dominant Professional Values</label></br>
+          <p><% pv["8"]["name"] %> <strong><% pv["8"]["percent"] %>%</strong><p>
+          <p><% pv["7"]["name"] %> <strong><% pv["7"]["percent"] %>%</strong><p>
+          <p><% pv["6"]["name"] %> <strong><% pv["6"]["percent"] %>%</strong><p>
         </div>
       </div>
       <div class="row">
       <div class="col-lg-6 col-md-6 col-sm-6 text-center">
-        <lable>Key Strengths</label></br>
-        <p><% ks["0"]["name"] %> <% ks["0"]["percent"] %>%<p>
-        <p><% ks["1"]["name"] %> <% ks["1"]["percent"] %>%<p>
-        <p><% ks["2"]["name"] %> <% ks["2"]["percent"] %>%<p>
+        <label class="pos-label">Key Strengths</label></br>
+        <p><% ks["0"]["name"] %> <strong><% ks["0"]["percent"] %>%</strong><p>
+        <p><% ks["1"]["name"] %> <strong><% ks["1"]["percent"] %>%</strong><p>
+        <p><% ks["2"]["name"] %> <strong><% ks["2"]["percent"] %>%</strong><p>
       </div>
       <div class="col-lg-6 col-md-6 col-sm-6 text-center">
-        <lable>Caution Areas</label></br>
-        <p><% ca["0"]["name"] %> <% ca["0"]["percent"] %>%<p>
-        <p><% ca["1"]["name"] %> <% ca["1"]["percent"] %>%<p>
-        <p><% ca["2"]["name"] %> <% ca["2"]["percent"] %>%<p>
+        <label class="neg-label">Caution Areas</label></br>
+        <p><% ca["0"]["name"] %> <strong><% ca["0"]["percent"] %>%</strong><p>
+        <p><% ca["1"]["name"] %> <strong><% ca["1"]["percent"] %>%</strong><p>
+        <p><% ca["2"]["name"] %> <strong><% ca["2"]["percent"] %>%</strong><p>
       </div>
       </div>
     </div>
