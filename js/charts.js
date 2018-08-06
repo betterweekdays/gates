453b4a337ae68bcf6ecaa25d69f976dd45bb2a19
Ann Zhou <ann.zhou@wustl.edu>
2018-08-06 11:48:25 -0500

    issue #17: added tooltip content

--- js/charts.js
+++ js/charts.js
@@ -1,14 +1,14 @@
 //customized info for dominant value (dv) dvDescription taken from JobScript sample report
 var dvDescription = [
-      {"name":"Motivational Energy","dx":"Individual has tremendous enthusiasm and motivation to share excitement with others, often gets people to enjoy the moment."},
-      {"name":"Strategic Decisions","dx":"Individual’s capacity to see the big picture and develop logical and effective strategies positions them to manage and advise."},
-      {"name":"Entrepreneurial Challenge","dx":"Individual is motivated by excitement and competition, entrepreneurial and challenging circumstances."},
-      {"name":"Natural Appreciation","dx":"Individual is a keen observer of life who wants to ensure the natural order is respected and maintained."},
-      {"name":"Production Efficiency","dx":"Individual is action-oriented and motivated by hard work and determination to get things done, the classic “doer” who can be counted on."},
-      {"name":"Artistic Creativity","dx":"Individual has a desire to be creative, has insight and an artistic nature leading to innovative designs, products and works of art."},
-      {"name":"Human Development","dx":"Individual is compassionate and service oriented, responds to the developmental needs of those around them."},
-      {"name":"Societal Change","dx":"Individual cares about things that affect society and wants to work to effect change and influence opinions toward the common good."},
-      {"name":"Theoretical Discovery","dx":"Individual seeks out underlying reasons things happen and likes to investigate and solve complex problems."}];
+      {"name":"Helping People","dx":"Students areasofstudy compassionate, service oriented and respond to the developmental needs of those around them."},
+      {"name":"Constantly Being Curious","dx":"Students seek out underlying reasons why things happen and investigate complex problems."},
+      {"name":"Creating New Things","dx":"Students have a desire to be creative and artistic leading to innovative designs, products and works of art."},
+      {"name":"Motivating Others","dx":"Students have tremendous enthusiasm and motivation to share excitement with others, often get people to enjoy the moment."},
+      {"name":"Effecting Change","dx":"Students care about things that affect society, create change and influence opinions toward the common good."},
+      {"name":"Keeping Perspective","dx":"Students are keen observers of life who want to ensure the natural order is respected and maintained."},
+      {"name":"Getting Things Done","dx":"Students are action-oriented, motivated by hard work and determination to get things done; classic “doers” who can be counted on."},
+      {"name":"Seeing the Big Picture","dx":"Students have capacity to see the big picture and develop effective strategies to manage projects."},
+      {"name":"Taking Risks","dx":"Students are excited by challenging entrepreneurial circumstances."}];
 
 var dvVue = new Vue({
   el: '#dv',
@@ -73,15 +73,15 @@ var dvVue = new Vue({
 
 //customized vue instance for key strength (ks)
 var ksDescription = [
-  {"name":"Accomplishment-oriented","dx":"KKKKKThis should be description for accomplishment-oriented"},
-  {"name":"Kind-hearted","dx":"SSSSSSdes1"},
-  {"name":"Change-oriented","dx":"KSKSKSKSdes2"},
-  {"name":"test1","dx":"des"},
-  {"name":"test2","dx":"des"},
-  {"name":"test3","dx":"des"},
-  {"name":"test4","dx":"des"},
-  {"name":"test5","dx":"des"},
-  {"name":"test6","dx":"des"}];
+  {"name":"Accomplishment-oriented","dx":"Students are practical-minded people who want to see tangible accomplishment from their efforts. Hands-on and no-nonsense in their approach will have them be responsible and effective."},
+  {"name":"Kind-hearted","dx":"Students are compassionate and kind-natured. Students will see the good in people and seek to help them when they are in trouble. Students' empathy helps them see others side of issues and their needs."},
+  {"name":"Change-oriented","dx":"Students like to instigate change. Students hold fairly strong opinions and will seek to influence how people see things, likely through their writings and words."},
+  {"name":"Quite Respectful","dx":"Students are watchful and observant and seek to ensure that a healthy balance is maintained. Students are patient, but will guard against injustice and unfair treatments if they see it occur."},
+  {"name":"Big-picture Thinker","dx":"Students like to see all sides of an issue and make the big decisions. Students want to think things through logically and integrate their findings into solutions. Often thinking they are right, students will seek to manage others to get things done."},
+  {"name":"Highly Motivated","dx":"Students are driven to succeed. Students will take on challenge and overcome obstacles in order to achieve their goals and gain the rewards that come from winning."},
+  {"name":"Very Outgoing","dx":"Students are exciting and enthusiastic. Students' energy will be infectious and help people to enjoy activities that they are involved with."},
+  {"name":"Definitely Innovative","dx":"Students are creative and likely independent in their thinking. Students like to try new things and invent unique ways of seeing something"},
+  {"name":"Very Inquisitive","dx":"Students are very curious and like to solve complex and intricate questions. Always wanting to know why something happens will have them explore facts and question reasons for things."}];
 
 var ksVue = new Vue({
   el: '#ks',
@@ -146,15 +146,15 @@ var ksVue = new Vue({
 
 //customized vue instance for caution areas (ca)
 var caDescription = [
-  {"name":"Accomplishment-oriented","dx":"caution area"},
-  {"name":"Kind-hearted","dx":"ccccccca"},
-  {"name":"Change-oriented","dx":"caaaaaaaa"},
-  {"name":"test1","dx":"des"},
-  {"name":"test2","dx":"des"},
-  {"name":"test3","dx":"des"},
-  {"name":"test4","dx":"des"},
-  {"name":"test5","dx":"des"},
-  {"name":"test6","dx":"des"}];
+  {"name":"Likely frustrated when not seeing results","dx":"Students work diligently to get things done on their own, but could be frustrated when working with others if things take too long. Be careful of showing their frustrations inappropriately."},
+  {"name":"Tend to take things personally","dx":"Because students care about people and want to have people like them, students may be offended by criticism, especially when it relates to how they are treating others."},
+  {"name":"Could hold on to anger","dx":"Students' high sense of integrity will cause them to want to be respectful and do things the right way. However, this could cause them to hold grudges toward those who are disrespectful."},
+  {"name":"Tend to always think you are right","dx":"Because students think things through and see all sides of an issue, students will have considerable confidence in their reasoned decisions and conclusions. Students may feel justified in thinking that people need to see things their way."},
+  {"name":"Might tend to compete with people","dx":"Students' drive and courage may have them compete to win, and they may fail to see when this approach is inappropriate to a given situation."},
+  {"name":"Might tend to talk too much","dx":"Students' high energy and dynamic nature may cause them to state their opinions too often. However, be careful of spending too much time sharing students' views."},
+  {"name":"Could be highly judgmental","dx":"Because students hold strong opinions, they will tend to judge people. Be careful of how this will be perceived and affect others."},
+  {"name":"Might over-analyze everything","dx":"Students want all the details before coming to conclusions. This may cause them to have difficulty with stopping their research and coming to a decision."},
+  {"name":"Might always want to get your way","dx":"Because students see creative visions and unique perspectives, they will assume that people should want the same things that they do."}];
 
 var caVue = new Vue({
   el: '#ca',
@@ -222,12 +222,12 @@ var jfdescription = [
     {"name":"Creating","dx":"Developing; Designing; Inventing"},
     {"name":"Marketing","dx":"Selling; Networking; Deal-making"},
     {"name":"Strategizing","dx":"Integrating; Problem-Solving; Advising"},
-    {"name":"test1","dx":"des"},
-    {"name":"test2","dx":"des"},
-    {"name":"test3","dx":"des"},
-    {"name":"test4","dx":"des"},
-    {"name":"test5","dx":"des"},
-    {"name":"test6","dx":"des"}];
+    {"name":"Explaining","dx":"des"},
+    {"name":"Researching","dx":"des"},
+    {"name":"Teaching","dx":"des"},
+    {"name":"Surveying","dx":"des"},
+    {"name":"Motivating","dx":"des"},
+    {"name":"Producing","dx":"des"}];
 
 var jfVue = new Vue({
   el: '#jf',
@@ -362,15 +362,15 @@ var jfVue = new Vue({
 //   {"name":"test6","dx":"des"}];
 
 var ocdescription = [
-  {"name":"Accomplishment-oriented","dx":"OC"},
-  {"name":"Kind-hearted","dx":"OCC"},
-  {"name":"Change-oriented","dx":"OCCC"},
-  {"name":"test1","dx":"des"},
-  {"name":"test2","dx":"des"},
-  {"name":"test3","dx":"des"},
-  {"name":"test4","dx":"des"},
-  {"name":"test5","dx":"des"},
-  {"name":"test6","dx":"des"}];
+  {"name":"Professional/Strategic","dx":""},
+  {"name":"Technical/Scientific","dx":""},
+  {"name":"Multi-cultural/Democratic","dx":""},
+  {"name":"Hands-on/Structured","dx":""},
+  {"name":"Creative/Experimental","dx":""},
+  {"name":"Entrepreneurial/High-Growth","dx":""},
+  {"name":"Natural/Respectful & Concerned","dx":""},
+  {"name":"Dynamic/Exciting","dx":""},
+  {"name":"Educational/Service-Oriented","dx":""}];
 
 var ocVue = new Vue({
     el: '#oc',
